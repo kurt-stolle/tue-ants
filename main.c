7637c20426c49989472bebfe46c0d048cdfe3101
Kurt Stolle <kurt@casualbananas.com>
2017-03-08 14:45:08 +0100

    Basic setup (2)

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+// This file contains the program's main method, i.e. the starting point of the program
+
+
+// Main function. Returns 0 if there was no error
 int main(){
+
+
+    // Return code 0 - no error
     return 0;
 }
